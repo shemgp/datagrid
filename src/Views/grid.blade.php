d3697b6f26b66f1a87f8f55ef57f51cad68ab1df
Shem Pasamba <shemgp@aiias.edu>
2017-01-25 16:20:43 +0800

    Fix display of header rows

--- src/Views/grid.blade.php
+++ src/Views/grid.blade.php
@@ -21,46 +21,12 @@
 
 				@foreach ($grid->getColumns() as $col)
 					@if ($col->isAction() === false)
-						@if ($col->hasFilters())
-							<th data-dg-col="{{ $col->getKey() }}" {!! $col->getAttributesHtml() !!}>
-								@if ( is_array($col->getFilters()) && count($col->getFilters()) > 0 )
-									{!!
-										Form::select(
-											$col->getFilterName(),
-											$col->getFilters(true),
-											$grid->getFilter($col->getKey()),
-											($col->hasFilterMany() ? ['multiple' => 'multiple'] : []) + array('class' => 'form-control input-sm', 'data-dg-type' => "filter")
-										)
-									!!}
-								@else
-									{!!
-										Form::text(
-											$col->getFilterName(),
-											$grid->getFilter($col->getKey()),
-											array('class' => 'form-control input-sm', 'data-dg-type' => "filter", 'placeholder' => $col->getTitle())
-										)
-									!!}
-								@endif
-							</th>
-						@else
-							<th data-dg-col="{{ $col->getKey() }}" {!! $col->getAttributesHtml() !!}>
-								@if ($col->isSortable())
-									@if ($row->dataIsNotNull($col->getKey(true)))
-										@if ($col->hasWrapper())
-											{!! $col->wrapper($row->{$col->getKey(true)}, $row) !!}
-										@else
-											{!! $row->{$col->getKey(true)} !!}
-										@endif
-									@endif
-								@else
-									{{ $col->getTitle() }}
-								@endif
-							</th>
-						@endif
-					@else
-						<th data-dg-col="actions">
-							<button class="btn btn-success btn-sm" type="submit" title="Search..."><i class="fa fa-search" aria-hidden="true"></i></button>
-							<a href="{{ \Request::url() }}" class="btn btn-danger btn-sm" title="Clear filters"><i class="fa fa-remove" aria-hidden="true"></i></a>
+						<th data-dg-col="{{ $col->getKey() }}" {!! $col->getAttributesHtml() !!}>
+							@if ($col->isSortable())
+								<a href="{{ Datagrid::getCurrentRouteLink($grid->getSortParams($col->getKey())) }}">{!! $col->getTitle() !!}<i class="glyphicon @if (\Input::get('f.order_by', '') == $col->getKey() && \Input::get('f.order_dir', 'ASC') == 'ASC') glyphicon-sort-by-attributes-alt @elseif (\Input::get('f.order_by', '') == $col->getKey() && \Input::get('f.order_dir', 'ASC') == 'DESC') glyphicon-sort-by-attributes @else glyphicon-sort @endif"></i></a>
+							@else
+								{{ $col->getTitle() }}
+							@endif
 						</th>
 					@else
 						<th data-dg-col="actions"><!-- Actions --></th>
